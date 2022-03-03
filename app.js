7c1d39b9ab0fb9b4f2407b26da33227fdb446920
Brinkley Hill <hillgangbro@gmail.com>
2022-03-03 13:00:54 -0500

    Update

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const mysql = require("mysql");
 
 const app = express();
-app.listen("3000", () => {
-  console.log("Server is running on Port 3000");
-});
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.render("login");
+});
+
+//have app listen on specifc port
+app.listen("3000", () => {
+  console.log("Server is running on Port 3000");
 });

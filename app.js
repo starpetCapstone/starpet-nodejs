d520a3699dc16ccdb77b302c2d0282425b147e1d
Brinkley Hill <hillgangbro@gmail.com>
2022-03-08 13:59:29 -0500

    jkjhkj

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const app = express();
 
+// create connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "GGGhhh442!",
+});
+
+// connec to database
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("MySql Connected");
+});
+
+//create DB
+app.get("/createdb", (req, res) => {
+  let sql = "CREATE DATABASE JARED";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("db created...");
+  });
+});
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
